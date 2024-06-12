05ba5e03c0abbac01ad3bbdf69dee1ea2f83c96d
toshota <toshota@student.42tokyo.jp>
2024-06-12 14:30:28 +0900

    [ref] add comment to see the step of the process

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tobeshota <tobeshota@student.42.fr>        +#+  +:+       +#+        */
+/*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 23:50:30 by tobeshota         #+#    #+#             */
-/*   Updated: 2024/06/11 23:50:30 by tobeshota        ###   ########.fr       */
+/*   Updated: 2024/06/12 14:29:57 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,39 @@
 #include "Fixed.hpp"
 
 int main(void) {
-  Fixed a;
-  Fixed b(a);
-  Fixed c;
+  Fixed a;                                  //  ①Default constructor called
+  Fixed b(a);                               //  ②Copy constructor called ③Copy assignment operator called ④getRawBits member function called
+  Fixed c;                                  //  ⑤Default constructor called
 
-  c = b;
+  c = b;                                    //  ⑥Copy assignment operator called ⑦getRawBits member function called
 
-  std::cout << a.getRawBits() << std::endl;
-  std::cout << b.getRawBits() << std::endl;
-  std::cout << c.getRawBits() << std::endl;
+  std::cout << a.getRawBits() << std::endl; //  ⑧getRawBits member function called ⑨0
+  std::cout << b.getRawBits() << std::endl; //  ⑩getRawBits member function called ⑪0
+  std::cout << c.getRawBits() << std::endl; //  ⑫getRawBits member function called ⑬0
 
-  return 0;
+  return 0;                                 //  ⑭Destructor called ⑮Destructor called ⑯Destructor called
 }
+
+
+/* 出力結果
+
+$> ./a.out
+①Default constructor called
+②Copy constructor called
+③Copy assignment operator called
+④getRawBits member function called
+⑤Default constructor called
+⑥Copy assignment operator called
+⑦getRawBits member function called
+⑧getRawBits member function called
+⑨0
+⑩getRawBits member function called
+⑪0
+⑫getRawBits member function called
+⑬0
+⑭Destructor called
+⑮Destructor called
+⑯Destructor called
+$>
+
+*/
